6e4fbbd7815197ce520df64d73901696bac23e2f
CozREV <kristianafg@proton.me>
2026-07-04 11:13:08 +0200

    added fonts, styled buttons
    styled the main menu buttons, added 2 fonts that is japanese-like

--- MainMenu/menuView.js
+++ MainMenu/menuView.js
@@ -1,18 +1,16 @@
 function MenuView()  {
     return /*html*/ `
-        <main>
-            <header id="main-title">
-                <h1 class="title">PackPon</h1>
-            </header>
-            <div id="desc">
-                <h3>All-In-One Japan Trip Planner</h3>
-            </div>
-            <nav>
-                <button class="menu-btn">Packer</button>
-                <button class="menu-btn">Budget</button>
-                <button class="menu-btn">Converter</button>
-            </nav>
-        </main>
+        <header id="main-title">
+            <h1 class="title">PackPon</h1>
+        </header>
+        <div id="desc">
+            <h3>All-In-One Japan Trip Planner</h3>
+        </div>
+        <nav id="btns">
+            <div class="menu-btn" id="packer-btn">Packer</div>
+            <div class="menu-btn" id="convert-btn">Converter</div>
+            <div class="menu-btn" id="budget-btn">Budget</div>
+        </nav>
     `
 }
 
